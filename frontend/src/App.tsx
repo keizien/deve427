c9ae56d5747f7cf1085f83fe8a0aec0db87c2f44
Salingue Clément <clement.salingue@ecoles-epsi.net>
2026-04-23 10:02:13 +0200

    feat: add cart display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import ProductList from './ProductList';
 import { CartProvider } from './CartContext';
+import Cart from './Cart';
 
 const App = () => (
   <CartProvider>
     <div>
       <ProductList />
+      <Cart />
     </div>
   </CartProvider>
 );
